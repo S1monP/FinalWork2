f665171f724ccd69b2dad7d9677cd0622f7f258a
simonpozigun <semen_pozigun@mail.ru>
2019-08-22 02:08:12 +0300

    Totally new
    Fixed animation
    Added answers
    Added sensor control

--- app/src/main/java/com/example/sandbox/AnswerData.java
+++ app/src/main/java/com/example/sandbox/AnswerData.java
@@ -1,7 +1,23 @@
 package com.example.sandbox;
 
+import java.util.Random;
+
 public class AnswerData {
     String[] answer = new String[5];
 
+    AnswerData(){
+        answer[0] = "ДА!";
+        answer[1] = "Нет!";
+        answer[2] = "Возможно";
+        answer[3] = "Не стоит";
+        answer[4] = "Попробуй!";
+    }
 
+    public String getAnswer(){
+        Random rand = new Random();
+
+// Obtain a number between [0 - 49].
+        int n = rand.nextInt(4);
+        return answer[n];
+    }
 }
--- app/src/main/java/com/example/sandbox/MainActivity.java
+++ app/src/main/java/com/example/sandbox/MainActivity.java
@@ -1,66 +1,104 @@
 package com.example.sandbox;
 
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.os.Handler;
-import android.os.SystemClock;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+
+    AnimationDrawable anim;
+ //   ImageView foto;
+    ImageView imageView;
+    TextView answer;
+    private SensorManager sensorManager;
+    private Sensor move;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
      super.onCreate(savedInstanceState);
-     setContentView(R.layout.activity_main);
-     final TextView text = findViewById(R.id.TextView);
-     text.setText("Hello");
+      setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        move = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     public void AnimeButton(View view) {
-        final TextView text = findViewById(R.id.TextView);
-        final ImageView foto = findViewById(R.id.imageView);
-        text.setText("POPOPOPOPOOPOO");
-//        long startTime = 0L;
-//        Handler customHandler = new Handler();
-//        long timeInMilliseconds = 0L;
-//        long timeSwapBuff = 0L;
-//        long updatedTime = 0L;
-        text.setTextColor(getResources().getColor(R.color.red));
-        foto.setImageDrawable(getResources().getDrawable(R.drawable.ball1));
-        view.buildDrawingCache();
-            long startTime = 0;
-            startTime = SystemClock.uptimeMillis();
-            do {
-                if (SystemClock.uptimeMillis() - startTime  > 2000){
-                    text.setTextColor(getResources().getColor(R.color.blue));
-                    foto.setImageDrawable(getResources().getDrawable(R.drawable.ball2));
-                    view.buildDrawingCache();
-                    break;
-                }
-            }while (SystemClock.uptimeMillis() - startTime  != 2000);
-            startTime = SystemClock.uptimeMillis();
-        do {
-            if (SystemClock.uptimeMillis() - startTime  > 2000){
-                text.setTextColor(getResources().getColor(R.color.red));
-                foto.setImageDrawable(getResources().getDrawable(R.drawable.ball3));
-                view.buildDrawingCache();
-                break;
+        imageView = findViewById(R.id.imageView);
+        final AnswerData answerData = new AnswerData();
+        answer = findViewById(R.id.textView);
+        // foto  = findViewById(R.id.imageView);
+        answer.setText("");
+        if(imageView == null) throw new AssertionError();
+        imageView.setBackgroundResource(R.drawable.ball_anim);
+        anim = (AnimationDrawable)imageView.getBackground();
+        anim.start();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                anim.stop();
+                answer.setText(answerData.getAnswer());
             }
-        }while (SystemClock.uptimeMillis() - startTime  != 2000);
-        startTime = SystemClock.uptimeMillis();
-        do {
-            if (SystemClock.uptimeMillis() - startTime  > 2000){
-                text.setTextColor(getResources().getColor(R.color.blue));
-                foto.setImageDrawable(getResources().getDrawable(R.drawable.ball4));
-                view.buildDrawingCache();
-                break;
+        }, 1100);
+
+    }
+
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+
+    @Override
+    public void onSensorChanged (SensorEvent event){
+        Sensor mySensor = event.sensor;
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float mov = event.values[0];
+            if (mov > 4.0){
+                imageView = findViewById(R.id.imageView);
+                final AnswerData answerData = new AnswerData();
+                answer = findViewById(R.id.textView);
+                // foto  = findViewById(R.id.imageView);
+                answer.setText("");
+                if(imageView == null) throw new AssertionError();
+                imageView.setBackgroundResource(R.drawable.ball_anim);
+                anim = (AnimationDrawable)imageView.getBackground();
+                anim.start();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        anim.stop();
+                        answer.setText(answerData.getAnswer());
+                    }
+                }, 1100);
             }
-        }while (SystemClock.uptimeMillis() - startTime  != 2000);
-        startTime = SystemClock.uptimeMillis();
-//            text.setTextColor(getResources().getColor(R.color.red));
-//            foto.setImageDrawable(getResources().getDrawable(R.drawable.ball2));
         }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, move, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
 }
 
